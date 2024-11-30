381be6812eef1e09a178717af93aea1097497e8a
PedroPiana <132151109+PedroPiana@users.noreply.github.com>
2024-11-30 17:14:16 -0300

    Update ladino.py

--- ladino.py
+++ ladino.py
@@ -1,11 +1,23 @@
-# personagem.py
-class Ladino:
-    def __init__(self, nome):
-        self.nome = nome
-        self.CA = None
-        self.HP = 50
+import random
 
-    def ataque(self ,personagemInimigo):
-        return 10
-    def setVida(self, vida):
-        self.HP = vida
+class Ladino:
+    def __init__(self):
+        self.HP = 75  # HP médio
+        self.CA = 14  # Boa evasão
+        self.dano_fisico = 10  # Ataque preciso
+    
+    def ataqueAcerto(self):
+        d20 = random.randint(1, 20)
+        dano = random.randint(1, self.dano_fisico) + 3  # Bônus de precisão
+        return f"AM{d20:02d}{dano:02d}"  # Ataque Físico
+
+    def getVida(self):
+        return self.HP
+    
+    def setVida(self, novaVida):
+        self.HP = novaVida
+
+    def especial(self):
+        evasao = random.randint(1, 10)  # Aumenta CA temporariamente
+        self.CA += evasao
+        print(f"{self.__class__.__name__} ficou em modo FURTIVO! Sua CA aumentou em {evasao} por um turno.")
