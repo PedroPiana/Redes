9824315191909cf8ca69beb5c201cfe882cd98c0
PedroPiana <132151109+PedroPiana@users.noreply.github.com>
2024-11-30 17:04:20 -0300

    Update clerigo.py

--- clerigo.py
+++ clerigo.py
@@ -4,9 +4,21 @@ class Clerigo:
         self.nome = nome
         self.CA = None
         self.HP = 100
+        self.dano_fisico = 8
+        self.dano_magico = 15
+    
+    def ataqueAcerto(self):
+        tipo_ataque = random.choice(['AD', 'AM'])  # Alterna entre mágico e físico
+        d20 = random.randint(1, 20)
+        if tipo_ataque == 'AM':  # Ataque Físico
+            dano = random.randint(1, self.dano_fisico)
+            return f"{tipo_ataque}{d20:02d}{dano:02d}"
+        else:  # Ataque Mágico
+            dano = random.randint(1, self.dano_magico)
+            return f"{tipo_ataque}{d20:02d}{dano:02d}"
 
-    def ataque(self ,personagemInimigo):
-        return 10
-    def setVida(self, vida):
-        self.HP = vida
-
+    def getVida(self):
+        return self.HP
+    
+    def setVida(self, novaVida):
+        self.HP = novaVida
